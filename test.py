b9a1b431e84b232804dd9ed805ce0a8032acf5b4
Keidan (K. Billonneau) <kuso.fr@gmail.com>
2022-03-06 01:34:47 +0100

    Fix codefactor report

--- test.py
+++ test.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 import argparse, sys, os, subprocess
 
-testNum = 1
 SAMPLE1 = "sample1.txt"
 SAMPLE2 = "sample2.txt"
 SAMPLE3 = "sample3.txt"
@@ -10,17 +9,18 @@ RESULT1 = "00000000004030000000000000203000"
 RESULT2 = "00000000005030000000000000603000"
 RESULT3 = "00000000005030000000000000603000"
 
-def testPass(label):
-  global testNum
-  print("Test \033[1m{0}\033[0m \033[38;5;208m{1}\033[0m \033[32mOK\033[0m".format(testNum, label))
-  testNum = testNum + 1
+class Test:
+  def __init__(self):
+    self.testNum = 1
+  def testPass(self, label):
+    print("Test \033[1m{0}\033[0m \033[38;5;208m{1}\033[0m \033[32mOK\033[0m".format(self.testNum, label))
+    self.testNum = self.testNum + 1
+        
+  def testFail(self, label):
+    print("Test \033[1m{0}\033[0m \033[38;5;208m{1}\033[0m \033[31mFAILED\033[0m".format(self.testNum, label))
+    sys.exit(1)
     
-def testFail(label):
-  global testNum
-  print("Test \033[1m{0}\033[0m \033[38;5;208m{1}\033[0m \033[31mFAILED\033[0m".format(testNum, label))
-  sys.exit(1)
-    
-def exec(args):
+def execProcess(args):
   process = subprocess.Popen(args)
   stdout, stderr = process.communicate()
   exitCode = process.wait()
@@ -46,43 +46,44 @@ def main(argv):
     print("File {0} not found".format(args.file))
     sys.exit(1)
     
+  t = Test()
   curDir = os.getcwd()
   sampleDir = getSample(SAMPLE_TEMP)
-  retCode = exec([args.file, "-i", getSample(SAMPLE1), "-o", sampleDir, "-s", "6", "-l", "47"])
+  retCode = execProcess([args.file, "-i", getSample(SAMPLE1), "-o", sampleDir, "-s", "6", "-l", "47"])
   if retCode != 0:
     os.remove(sampleDir)
-    testFail("sample1 exec")
+    t.testFail("sample1 exec")
 
-  testPass("sample1 exec   ")
+  t.testPass("sample1 exec   ")
   result = readFile(sampleDir)
   if result != RESULT1:
     os.remove(sampleDir)
-    testFail("sample1 compare")
+    t.testFail("sample1 compare")
 
-  testPass("sample1 compare")
-  retCode = exec([args.file, "-i", getSample(SAMPLE2), "-o", sampleDir, "-l", "47"])
+  t.testPass("sample1 compare")
+  retCode = execProcess([args.file, "-i", getSample(SAMPLE2), "-o", sampleDir, "-l", "47"])
   if retCode != 0:
     os.remove(sampleDir)
-    testFail("sample2 exec")
+    t.testFail("sample2 exec")
 
-  testPass("sample2 exec   ")
+  t.testPass("sample2 exec   ")
   result = readFile(sampleDir)
   if result != RESULT2:
     os.remove(sampleDir)
-    testFail("sample2 compare")
+    t.testFail("sample2 compare")
 
-  testPass("sample2 compare")
-  retCode = exec([args.file, "-i", getSample(SAMPLE3), "-o", sampleDir, "-s", "1"])
+  t.testPass("sample2 compare")
+  retCode = execProcess([args.file, "-i", getSample(SAMPLE3), "-o", sampleDir, "-s", "1"])
   if retCode != 0:
     os.remove(sampleDir)
-    testFail("sample3 exec")
+    t.testFail("sample3 exec")
 
-  testPass("sample3 exec   ")
+  t.testPass("sample3 exec   ")
   if result != RESULT3:
     os.remove(sampleDir)
-    testFail("sample3 compare")
+    t.testFail("sample3 compare")
     
-  testPass("sample3 compare")
+  t.testPass("sample3 compare")
   print("TEST \033[32mPASSED\033[0m")
   os.remove(sampleDir)
   
