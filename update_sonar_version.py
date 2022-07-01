72b9e30c5e364b77282f607ae1b5178c9a150294
Keidan (K. Billonneau) <kuso.fr@gmail.com>
2022-07-01 23:02:30 +0200

    #3: Sonarcloud support

---
+++ update_sonar_version.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+import argparse, sys, os, re
+
+VERSION="version.txt"
+SONAR_PROJECT="sonar-project.properties"
+
+def readFile(filename):
+  lines = ""
+  with open(filename) as f:
+    lines += f.read()
+  return lines
+
+def main(argv):
+  parser = argparse.ArgumentParser()
+  parser.add_argument("-r", "--root", help="Root folder.")
+  args = parser.parse_args()
+  version = ""
+  sonar_project = ""
+  if len(args.root) != 0:
+    version = os.path.join(args.root, VERSION)
+    sonar_project = os.path.join(args.root, SONAR_PROJECT)
+  else:
+    version = VERSION
+    sonar_project = SONAR_PROJECT
+
+  if not os.path.exists(version):
+    print("File {0} not found".format(version))
+    sys.exit(1)
+  if not os.path.exists(sonar_project):
+    print("File {0} not found".format(sonar_project))
+    sys.exit(1)
+    
+  major = ""
+  minor = ""
+  result = re.search(r"VERSION_MAJOR ([0-9]*)\nVERSION_MINOR ([0-9]*)", readFile(version))
+  major = result.group(1)
+  minor = result.group(2)
+  if len(major) == 0:
+    print("Major version number not found")
+    sys.exit(1)
+  if len(minor) == 0:
+    print("Minor version number not found")
+    sys.exit(1)
+  vers = major + "." + minor
+  content = readFile(sonar_project)
+  result = re.search(r"([\s\S]+sonar\.projectVersion=)[0-9]*\.[0-9]*([\s\S]+)", content)
+  new_content = result.group(1) + vers + result.group(2)
+
+  if new_content != content:
+    print("Update sonar-project version")
+    with open(sonar_project, "w") as f:
+      f.write(new_content)
+
+if __name__ == '__main__':
+  main(sys.argv[1:])
