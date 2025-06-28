e52e6c18ff41fb418b2307182b95d73e6102ef7f
Keidan (K. Billonneau) <3134348+Keidan@users.noreply.github.com>
2025-06-28 02:44:20 +0200

    gcov config

--- cmakes/gcov.cmake
+++ cmakes/gcov.cmake
@@ -1,20 +1,20 @@
 
 if(DEFINED IS_DEBUG AND IS_DEBUG EQUAL 1 AND NOT DEFINED WIN32)
+  set(OBJECT_DIR ${CMAKE_BINARY_DIR}/CMakeFiles/${CMAKE_PROJECT_NAME}.dir)
   # Create the gcov target. Run coverage tests with 'make gcov'
   add_custom_target(gcov
     COMMAND mkdir -p coverage
-    WORKING_DIRECTORY ${H2B_ROOT_BIN_DIR}
+    WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
   )
-  set(OBJECT_DIR ${H2B_ROOT_BIN_DIR}/CMakeFiles/${H2B_PROJECT_NAME}.dir/src)
   add_custom_command(TARGET gcov
-    COMMAND gcov -b ${hex2bin_src}.gcno -o ${OBJECT_DIR}
-    COMMAND gcov -b ${intelhex_src}.gcno -o ${OBJECT_DIR}
-    COMMAND gcov -b ${parameters_src}.gcno -o ${OBJECT_DIR}
-    COMMAND gcov -b ${files_src}.gcno -o ${OBJECT_DIR}
-    COMMAND gcov -b ${main_src}.gcno -o ${OBJECT_DIR}
-    WORKING_DIRECTORY ${H2B_ROOT_BIN_DIR}/coverage
+    COMMAND gcov -b ${CMAKE_SOURCE_DIR}/src/ihex/Parameters.cpp.gcno -o ${OBJECT_DIR}/src/ihex/
+    COMMAND gcov -b ${CMAKE_SOURCE_DIR}/src/Files.cpp.gcno -o ${OBJECT_DIR}/src/
+    COMMAND gcov -b ${CMAKE_SOURCE_DIR}/src/Hex2Bin.cpp.gcno -o ${OBJECT_DIR}/src/
+    COMMAND gcov -b ${CMAKE_SOURCE_DIR}/src/IntelHex.cpp.gcno -o ${OBJECT_DIR}/src/
+    COMMAND gcov -b ${CMAKE_SOURCE_DIR}/src/main.cpp.gcno -o ${OBJECT_DIR}/src/
+    WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/coverage
   )
-  add_dependencies(gcov ${H2B_PROJECT_NAME})
+  add_dependencies(gcov ${CMAKE_PROJECT_NAME})
 
   # Make sure to clean up the coverage folder
   set_property(DIRECTORY APPEND PROPERTY ADDITIONAL_MAKE_CLEAN_FILES coverage)
@@ -23,8 +23,8 @@ if(DEFINED IS_DEBUG AND IS_DEBUG EQUAL 1 AND NOT DEFINED WIN32)
   # .gcda and .gcno files.
   add_custom_target(scrub
     COMMAND ${CMAKE_MAKE_PROGRAM} clean
-    COMMAND rm -f ${OBJECT_DIR}/*.gcno
-    COMMAND rm -f ${OBJECT_DIR}/*.gcda
-    WORKING_DIRECTORY ${H2B_ROOT_BIN_DIR}/coverage
+    COMMAND find ${OBJECT_DIR} -type f -name "*.gcno" | xargs rm 2>/dev/null
+    COMMAND find ${OBJECT_DIR} -type f -name "*.gcda" | xargs rm 2>/dev/null
+    WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
   )
 endif()
